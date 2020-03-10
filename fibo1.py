caeb46c152888569b3d6d45cbcef421ddf3df0fb
bambi <sarnina@me.com>
2020-03-10 19:00:14 +0100

    correct index output

--- fibo1.py
+++ fibo1.py
@@ -4,10 +4,10 @@ fi = []
 fi.append(fibo1)
 index = int(input("your number: "))
 
-for i in range(0, index + 1):
+for i in range(0, index - 1):
     fibo = fibo1 + fibo2
     fibo1 = fibo2
     fibo2 = fibo
     fi.append(fibo)
 
-print(fi[index + 1])
+print(fi[index - 1])
